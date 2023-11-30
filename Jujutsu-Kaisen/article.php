a219addf2b7c4f74b6fd7bdec1209477e334bc28
Muhammad Sultan Alhakim <54764007+msultanalhakim@users.noreply.github.com>
2023-11-30 22:27:54 +0700

    Update features Article (View All)

--- Jujutsu-Kaisen/article.php
+++ Jujutsu-Kaisen/article.php
@@ -118,4 +118,108 @@
     </div>
         <script src="assets/js/script.js"></script>
     </body>
-</html>
+</htm<?php
+    include "connection.php";
+    session_start();
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Jujutsu Kaisen - Shibuya Incident</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="icon" href="assets/icon/jujutsu-kaisen-highschool.ico"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body{background-color: rgba(13, 13, 13);}
+        </style>
+    </head>
+    <body>
+        <?php include('navbar.php'); ?>
+        <div class="article-page">
+            <div class="container-article">
+                <div class="sub-article">
+                    <h2>Article Lists</h2>
+                    <span><a href="index.php">Home</a> &gt; <a href="season.php">Season</a> &gt; <a href="season-one.php">Jujutsu Kaisen (TV)</a></span>
+                    <div class="article-item">
+                        <ul>
+                            <li><a href=""><img src="assets/images/articles/article-01.png" alt=""></a></li>
+                            <li class="right">
+                                <a href="" class="btn-news">News</a>
+                                <a href=""><h2>Yuji Itadori di Jujutsu Kaisen, Siapakah Dia?</h2></a>
+                                <span>September 24, 2023</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="article-item">
+                        <ul>
+                            <li><a href=""><img src="assets/images/articles/article-02.jpeg" alt=""></a></li>
+                            <li class="right">
+                                <a href="" class="btn-news">News</a>
+                                <a href=""><h2>Kematian Gojo Satoru, Begini Komentar Komikus!</h2></a></a>
+                                <span>September 25, 2023</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="article-item">
+                        <ul>
+                            <li><a href=""><img src="assets/images/articles/article-03.jpeg" alt=""></li>
+                            <li class="right">
+                                <a href="" class="btn-news">News</a>
+                                <a href=""><h2>Penjelasan Akhir Film Jujutsu Kaisen!</h2></a>
+                                <span>March 24, 2023</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="article-pagination">
+                        <a href="" class="btn-pagination">&lt;</a>
+                        <a href="" class="btn-pagination">2</a>
+                        <a href="" class="btn-pagination">3</a>
+                        <a href="" class="btn-pagination">4</a>
+                        <a href="" class="btn-pagination">&gt;</a>
+                    </div>
+                </div>
+                <!--
+                <div class="sidebar-article">
+                    <div class="sidebar-content">
+                        <h2>Popular Article</h2>
+                        <div class="sidebar-item">
+                            <a href=""><ul>
+                                <li><img src="assets/images/articles/article-01.png"></li>
+                                <li><h4>Yuji Itadori di Jujutsu Kaisen, siapakah dia?</h4></li>
+                                <li><span>September 27, 2023</span></li>
+                            </ul></a>
+                        </div>
+                        <div class="sidebar-item">
+                            <a href=""><ul>
+                                <li><img src="assets/images/articles/article-02.jpeg"></li>
+                                <li><h4>Kematian Gojo Satoru, Begini Komentar Komikus!</h4></li>
+                                <li><span>September 25, 2023</span></li>
+                            </ul></a>
+                        </div>
+                        <div class="sidebar-item">
+                            <a href=""><ul>
+                                <li><img src="assets/images/articles/article-03.jpeg"></li>
+                                <li><h4>Penjelasan Akhir Film Jujutsu Kaisen!</h4></li>
+                                <li><span>March 25, 2023</span></li>
+                            </ul></a>
+                        </div>
+                        <div class="sidebar-item">
+                            <a href=""><ul>
+                                <li><img src="assets/images/articles/article-01.png"></li>
+                                <li><h4>Yuji Itadori di Jujutsu Kaisen, siapakah dia?</h4></li>
+                                <li><span>27 December 2023</span></li>
+                            </ul></a>
+                        </div>
+                    </div>
+                </div>-->
+        </div>
+    </div>
+    <div class="footer" id="footer">
+        &copy; Jujutsu Kaisen 2023. All rights reserved.
+    </div>
+        <script src="assets/js/script.js"></script>
+    </body>
+</htm
